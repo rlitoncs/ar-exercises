ff09dde904f707fbd2b7895a20cc396e5722444f
rlitoncs <rj_liton@hotmail.com>
2024-09-24 17:07:29 -0400

    Feat: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# Find store
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Update Store
+@store1.update(name: "Bobby Junior") # update name
+
+#alternative:
+#@store1.name = "Bobby Junior"
+#@store1.save
+
